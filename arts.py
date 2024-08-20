7a7ccac9b8a135af89008b9b4b27fa2244302786
Yara Alharbi <151997489+ytrbi@users.noreply.github.com>
2024-08-20 07:46:05 +0300

    v1.0.1 | Added ASCII art for Hangman stages

--- arts.py
+++ arts.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+# Title art
+>>>>>>> v1.0.1 | Added ASCII art for Hangman stages
 title_art = """
 .---.  .---.    ____    ,---.   .--.  .-_'''-.   ,---.    ,---.   ____    ,---.   .--. 
 |   |  |_ _|  .'  __ `. |    \  |  | '_( )_   \  |    \  /    | .'  __ `. |    \  |  | 
@@ -10,5 +14,81 @@ title_art = """
 '(_,_) '---'  '.(_,_).' '--'    '--'   `'-...-'  '--'      '--' '.(_,_).' '--'    '--' 
 """
 
+<<<<<<< HEAD
 
 # print(title_art)
+=======
+# Gallows art by incorrect guesses
+hangman_stages = {
+    0: """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+    1: """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    2: """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    3: """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+""",
+    4: """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    5: """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    6: """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+"""
+}
+
+# Function to display the hangman art based on incorrect guesses
+def display_hangman(incorrect_guesses):
+    print(title_art)
+    print(hangman_stages.get(incorrect_guesses, hangman_stages[6]))  # Defaults to complete figure if out of range
+
+
+>>>>>>> v1.0.1 | Added ASCII art for Hangman stages
