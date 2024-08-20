fb60ddb3e46bed49089da684532cb6675da2bcf1
Yara Alharbi <151997489+ytrbi@users.noreply.github.com>
2024-08-20 07:48:46 +0300

    v1.0.2 | Moved word list to a separate file for better organization

---
+++ words_list.py
@@ -0,0 +1,4 @@
+words = ['luxury', 'quiz', 'icebox', 'juicy', 'voodoo', 'cycle', 'equip', 'pajama',
+         'oxygen', 'pixel', 'galaxy', 'funny', 'zigzag', 'staff', 'scratch', 'jazz',
+         'mouse', 'jungle', 'notebook', 'frug', 'pizza', 'rocket', 'calendar',
+         'algebra', 'chocolate', 'koala', 'unicorn', 'father']
