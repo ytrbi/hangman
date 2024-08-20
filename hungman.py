696472f39c87e083c78b296d2f1059489fd8914f
Yara Alharbi <151997489+ytrbi@users.noreply.github.com>
2024-08-20 07:46:46 +0300

    v1.0.1 | Added ASCII art for Hangman stages

--- hungman.py
+++ hungman.py
@@ -1,30 +1,40 @@
 import random
-from arts import title_art
+import arts
 
-print(title_art)
+print(arts.title_art)
 
-words = ['luxury', 'quiz', 'icebox', 'juicy', 'voodoo', 'cycle', 'equip', 'pajama', 'oxygen', 'pixel', 'galaxy', 'funny', 'zigzag', 'staff', 'scratch', 'jazz', 'mouse', 'jungle', 'notebook', 'frug', 'pizza', 'rocket', 'calendar', 'algebra', 'chocolate', 'koala', 'unicorn', 'father']
+words = ['luxury', 'quiz', 'icebox', 'juicy', 'voodoo', 'cycle', 'equip', 'pajama', 
+         'oxygen', 'pixel', 'galaxy', 'funny', 'zigzag', 'staff', 'scratch', 'jazz', 
+         'mouse', 'jungle', 'notebook', 'frug', 'pizza', 'rocket', 'calendar', 
+         'algebra', 'chocolate', 'koala', 'unicorn', 'father']
+
 secret_word = random.choice(words)
-
 blanks = ['_'] * len(secret_word)
-tempts = 0 
+incorrect_attempts = 0
+max_attempts = 6
 
+print(arts.hangman_stages[0])
 print(f'The word length is {len(secret_word)}')
 print(f'You have to guess the word: {" ".join(blanks)}')
 
-
-while tempts <= 8 :
+while incorrect_attempts < max_attempts:
     user_guess = input('Make a guess: ').lower()
-    tempts += 1
 
-    for i in range(len(secret_word)):
-        if secret_word[i] == user_guess:
-            blanks[i] = user_guess
-    if '_' in blanks:
-        print(f'You have {8 - tempts} attempts left')
-    elif  tempts == 8:
-        print('You lost! The word was: ' + secret_word)
+    if user_guess in secret_word:
+        for i in range(len(secret_word)):
+            if secret_word[i] == user_guess:
+                blanks[i] = user_guess
     else:
+        incorrect_attempts += 1
+
+    arts.display_hangman(incorrect_attempts)
+
+    if '_' not in blanks:
         print('You guessed the word!')
         break
+
     print(f'Updated word: {" ".join(blanks)}')
+    print(f'You have {max_attempts - incorrect_attempts} attempts left')
+
+if incorrect_attempts == max_attempts:
+    print('You lost! The word was: ' + secret_word)
